2d329ca60664ae2c378dd289e243debf5079079f
Akash <akashpoddar492@gmail.com>
2026-03-12 19:05:17 +0530

    create ProductDao with saveProduct, findProductByid, GetAll Products and delete products by id methods

--- src/main/java/com/practice/dao/ProductDao.java
+++ src/main/java/com/practice/dao/ProductDao.java
@@ -1,6 +1,10 @@
 package com.practice.dao;
 
+import com.practice.entity.Product;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class ProductDao {
 
@@ -9,5 +13,33 @@ public class ProductDao {
     public ProductDao(EntityManager em) {
         this.em = em;
 
+    }
+
+    //Add Product
+    public void saveProduct(Product product) {
+        em.persist(product);
+    }
+
+    //Find Product By Id
+    public Product getProduct(Long id) {
+        return em.find(Product.class, id);
+    }
+
+    //Get all Products
+    public List<Product> getAllProducts() {
+        TypedQuery<Product> query = em.createQuery("SELECT p FROM  Product p", Product.class);
+        List<Product> resultList = query.getResultList();
+        return resultList;
+    }
+
+
+    //Delete Product by id
+    public void deleteProduct(Long id) {
+        Product product = em.find(Product.class, id);
+        if (product != null) {
+            em.remove(product);
+        } else {
+            System.out.println("Invalid Id");
+        }
     }
 }
