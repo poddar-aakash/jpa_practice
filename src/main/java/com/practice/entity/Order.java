908251a61842cf42895d698365fc2ca71ce1c001
Akash <akashpoddar492@gmail.com>
2026-03-12 18:34:45 +0530

    created pojo classes for product and order.

--- src/main/java/com/practice/entity/Order.java
+++ src/main/java/com/practice/entity/Order.java
@@ -1,8 +1,64 @@
 package com.practice.entity;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Date;
 
 @Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String customerName;
+
+
+    @CreationTimestamp
+    private Date orderDate;
+
+    public Order() {
+    }
+
+    public Order(Long id, String customerName, Date orderDate) {
+        this.id = id;
+        this.customerName = customerName;
+        this.orderDate = orderDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customerName='" + customerName + '\'' +
+                ", orderDate=" + orderDate +
+                '}';
+    }
 }
--- src/main/java/com/practice/entity/Product.java
+++ src/main/java/com/practice/entity/Product.java
@@ -1,8 +1,60 @@
 package com.practice.entity;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Long price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String productName, Long price) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
